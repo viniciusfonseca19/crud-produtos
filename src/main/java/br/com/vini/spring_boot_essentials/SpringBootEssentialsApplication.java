9a3bc9a1bc40e68879d1ba208ea4e3402191560a
Vinícius F. <fonsecavinicius117@gmail.com>
2026-07-07 07:29:29 -0300

    initial commit

---
+++ src/main/java/br/com/vini/spring_boot_essentials/SpringBootEssentialsApplication.java
@@ -0,0 +1,13 @@
+package br.com.vini.spring_boot_essentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEssentialsApplication {
+//metodo run
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEssentialsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/vini/spring_boot_essentials/service/ProdutoService.java
@@ -0,0 +1,7 @@
+package br.com.vini.spring_boot_essentials.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoService {
+}
